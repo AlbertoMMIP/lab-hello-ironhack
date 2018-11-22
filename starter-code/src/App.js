31f6b5ab965191ee4ec219f5740d8c2737bc3ebd
Alberto Martinez <jose_alberto12216@hotmail.com>
2018-11-21 22:47:01 -0600

    changes

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import React, {Component} from 'react';
-
-class App extends Component{
-    render(){
-        return (
-            <div >
-            <h1>Home</h1>
-            </div>
-    );
-=======
 import React, {Component} from "react";
 import Cards from "./Cards";
 import Main from "../src/Main";
@@ -21,7 +10,6 @@ class App extends Component{
                 <Cards />
             </div>
         );
->>>>>>> c5dd80edc252b000e7abf9db19fbd5ddb113450d
     }
 }
 
